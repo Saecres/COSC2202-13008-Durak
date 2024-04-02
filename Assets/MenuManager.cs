aaee174833042f599457e223a9466e615b5a42f1
Saecres <kaitlingracep04@gmail.com>
2024-04-02 00:05:44 -0400

    [Create] files - Created a Main Menu - Added an instructions and settings page - Designed the foundations for the game
    [Changelog]
    - Add more button functionality (reset)
    - Add better settings for the SettingsPanel
    - Design Card gameplay

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b9419644e15ff31cde85818df7e94a0b09e19f30d9d6bed30210fdab69df4adb
+size 192
---
+++ Assets/TextSlider.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d0668eea9c8d77c3743e4318b7daf414ec1b2a1285bd851862bf8181d26b2af6
+size 276
