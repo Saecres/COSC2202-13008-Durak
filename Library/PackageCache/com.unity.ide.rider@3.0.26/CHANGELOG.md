961909e36fabfd9d35e913f81af012ce1133551a
Manu-Sugu <156142501+Manu-Sugu@users.noreply.github.com>
2024-04-03 12:20:36 -0400

    Delete Library directory

--- Library/PackageCache/com.unity.ide.rider@3.0.26/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:add3a6e887ea88ed3986b08a40805684bc2187a0819f6d9f36e54fc375b8168c
-size 10450
--- Library/PackageCache/com.unity.ide.rider@3.0.26/Rider/Editor/RiderScriptEditor.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6220d030cad3bc23a6f843c3ba91950462cd985797bbbbc8e331cd128bbc401d
-size 15624
