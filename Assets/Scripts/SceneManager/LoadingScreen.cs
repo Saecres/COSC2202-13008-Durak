3e7e7cd63733604021279bee75804af351f32925
Kaitlin <kaitlingracep04@gmail.com>
2024-04-16 21:13:27 -0400

    [Create] Loading Screen
    - Implemented a new loading screen from 1P -> Gameplay
    - Fixed color leak on 1P game customization
    - Added a buffer to loading screen so it doesn't instantly load up

---
+++ Assets/Scripts/SceneManager/LoadingScreen.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public GameObject Loading;
+    public Image LoadingBarFill;
+
+    public void LoadScene(int sceneId)
+    {
+        StartCoroutine(LoadSceneAsync(sceneId));
+    }
+
+    IEnumerator LoadSceneAsync(int sceneId) 
+    {
+        // Console debugging
+        Debug.Log("Loading...");
+
+        Loading.SetActive(true);
+
+        // Start loading the scene asynchronously
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+
+        // Waits for 18 second
+        yield return new WaitForSeconds(1000.0f);
+
+        while (!operation.isDone) 
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+            LoadingBarFill.fillAmount = progressValue;
+
+            yield return null;
+        }
+    }
+}
